f4b5aafa86b4443eb84a5bf71d5fcb55cc072cb0
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-04-27 17:53:54 -0400

    Install dependencies

--- mix.exs
+++ mix.exs
@@ -22,8 +22,12 @@ defmodule I2cServer.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:circuits_i2c, "~> 0.3"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
+      {:mox, "~> 1.0.0", only: :test}
     ]
   end
 end
