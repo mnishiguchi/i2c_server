ff8c5e597c3d072656712ffb0c9eaeeea27994d9
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-05-20 13:29:10 -0400

    Always waring as errors

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,8 @@
+# Always warning as errors
+if Version.match?(System.version(), "~> 1.10") do
+  Code.put_compiler_option(:warnings_as_errors, true)
+end
+
 Application.put_env(:i2c_server, :transport_module, I2cServer.MockTransport)
 Mox.defmock(I2cServer.MockTransport, for: I2cServer.Transport)
 
