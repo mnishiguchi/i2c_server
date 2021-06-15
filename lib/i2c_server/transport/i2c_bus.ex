d4313fadc93bc957c21a7d3228102bea8ebc8d7f
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-06-15 19:49:00 -0400

    Refactor Mox-related code

--- lib/i2c_server/transport/i2c_bus.ex
+++ lib/i2c_server/transport/i2c_bus.ex
@@ -4,19 +4,19 @@ defmodule I2cServer.I2cBus do
   @behaviour I2cServer.Transport
 
   def open(bus_name) do
-    apply(transport_module(), :open, [bus_name])
+    transport_module().open(bus_name)
   end
 
   def write(reference, bus_address, data) do
-    apply(transport_module(), :write, [reference, bus_address, data])
+    transport_module().write(reference, bus_address, data)
   end
 
   def read(reference, bus_address, read_count) do
-    apply(transport_module(), :read, [reference, bus_address, read_count])
+    transport_module().read(reference, bus_address, read_count)
   end
 
   def write_read(reference, bus_address, data, read_count) do
-    apply(transport_module(), :write_read, [reference, bus_address, data, read_count])
+    transport_module().write_read(reference, bus_address, data, read_count)
   end
 
   defp transport_module() do
